0a10ba1842744cae8a94f7be46c6cd3e44eb60aa
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-07-19 15:57:08 +0300

    ver.1.1

--- script.js
+++ script.js
@@ -7,33 +7,50 @@
 // sortWords('2Fiona Ig7or J1amila B3ob Ali5ce') // 'J1amila 2Fiona B3ob A3lice Ig7or';
 // sortWords('Tes1t') // 'Tes1t';
 
+var example1 = 'A3lice 2Bob Er1nst';
+var example2 = '2Fiona Ig7or J1amila B3ob Ali5ce';
+var example3 = 'Tes1t';
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+function getNum(str){
+    var arrNumber = [];
+    for(var i = 0; i < str.length; i++){
+        for(var j = 0; j < str[i].length; j++){
+            if (isNumeric(str[i].charAt(j))) {
+                arrNumber.push(str[i].charAt(j));
+            }
+        }
+    }
+    return arrNumber;
+}
+
+function getObject(str){
+    var getNumber = getNum(str);
+    var getArr = str.split(' ');
+    var arrObject = [];
+
+    for(var i = 0; i < getNumber.length; i++){
+        arrObject.push({key1 : getArr[i], key2 : getNumber[i]});
+    }
+    return arrObject;
+}
 
 function sortWords(str){
-    var getArr = str.split(' ');
-    var getWord = [];
-    for(var i = 0; i < getArr.length; i++){
-       getWord[i] = getArr[i].split('');
-        getWord[i].sort();
-        getWord[i] = getWord[i].join('');      
-    }
-    
-    var arrObject = [];
-    for (var i = 0; i < getArr.length; i++){
-        arrObject.push({key1 : getArr[i], key2 : getWord[i]})
-    }
-    
-    arrObject.sort (function (a,b){
+    var sorting = getObject(str).sort (function (a,b){
         if (a.key2 < b.key2) return -1;
     });
 
     var getSort = [];
 
-    for (var i = 0; i < arrObject.length; i++){
-        getSort.push (arrObject[i].key1);
+    for (var i = 0; i < sorting.length; i++){
+        getSort.push (sorting[i].key1);
     }
     return getSort.join(' ');
 }
 
-alert (sortWords('A3lice 2Bob Er1nst')); // 'Er1nst 2Bob A3lice';
-alert (sortWords('2Fiona Ig7or J1amila B3ob Ali5ce')); // 'J1amila 2Fiona B3ob A3lice Ig7or';
-alert (sortWords('Tes1t')); // 'Tes1t';
+console.log (sortWords(example1));
+console.log (sortWords(example2));
+console.log (sortWords(example3));
